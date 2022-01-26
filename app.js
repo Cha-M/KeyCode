84aea78b222babda1e74bb6c0e7c68d8d025231d
Sha <charlotte.megroff@outlook.com>
2022-01-26 13:55:04 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// const shaMessage = document.getElementById("testPara");
+// document.setAttribute("tabindex", 0);
+
+// testPara.addEventListener("click", () => {console.log("you clicked the test"); document.body.style.background = "blue";})
+
+// const func1 = (shaStr) => {
+//     console.log(shaStr);
+//     // alert("keydown");
+//     if (Event.keyCode == 86)
+//     {
+//         document.body.style.background = "violet";
+//     }
+      
+//     shaMessage.textcontent = shaStr;
+// }
+
+
+
+
+// // document.addEventListener("keydown", func());
+
+document.body.addEventListener("keydown", console.log("keydown"));
